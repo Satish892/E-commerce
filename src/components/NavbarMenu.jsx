6bd6861bf563da18b8c6899539a749b2a8a151e0
Satish892 <gongadasatish1999@gmail.com>
2024-08-15 00:35:16 +0530

    Update NavbarMenu.jsx

--- src/components/NavbarMenu.jsx
+++ src/components/NavbarMenu.jsx
@@ -45,7 +45,7 @@ const NavbarMenu = ({ handleShowCategorySidebar }) => {
                 width={50}
                 fluid="true"
                 className="d-inline-block align-top"
-                alt="QuickPik Logo"
+                alt="Billinore Logo"
               />
               <div className="d-flex flex-column justify-content-center">
                 <h4 className="m-0" style={{ fontSize: "1rem" }}>
